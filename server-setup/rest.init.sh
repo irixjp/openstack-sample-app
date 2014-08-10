6ee66d5c7c8ac62d03b97fa061cb729abfca9fe0
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2014-08-10 09:25:26 +0900

    add branch v1.0

--- server-setup/rest.init.sh
+++ server-setup/rest.init.sh
@@ -2,7 +2,42 @@
 
 . /etc/rc.d/init.d/functions
 
-cd $(dirname $0)
-cd ../
+retval=0
+prog="rest.py"
+logfile="rest.log"
+pidfile="/var/run/sample-app/rest.pid"
 
-daemon --user=root "python rest.py >> rest.log &"
+start () {
+    echo -n $"Starting $prog"
+
+    if [ ! -d /var/run/sample-app ]; then
+         mkdir -p /var/run/sample-app
+    fi 
+
+    daemon --pidfile ${pidfile} --user=root "cd $(dirname $0); cd ../; python $prog >> $logfile"
+    RETVAL=$?
+    echo
+
+    return $retval
+}
+
+case "$1" in
+    start)
+       start
+       $1
+    ;;
+    stop)
+       rh_status_q || exit 0
+       $1
+    ;;
+    restart)
+       $1
+    ;;
+    status)
+       rh_status
+    ;;
+    *)
+       echo $"Usage: $0 {start|stop|status|restart}"
+       exit 2
+esac
+
